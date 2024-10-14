960cbb89c22e9bfb733b5ee60bdb2cf4f1a01d42
Abhishek PM <abhishekmagadum7@gmail.com>
2024-10-14 10:24:14 +0530

    Test case 1 : Single number

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const stringCalculator =
     add: function(strNumbers)
     {
         //If string is empty
-        return 0 
+        if(strNumbers === "")
+            return 0 
+
+        //If string is a single number (no comma)
+        return Number(strNumbers);
     }
 }
 
--- index.test.js
+++ index.test.js
@@ -5,3 +5,9 @@ test('returns 0 if string is empty', ()  =>
     //test case 1: Empty string
     expect(stringCalculator.add("")).toBe(0);
 })
+
+test('returns number iteself, if string is a single number', ()  =>
+{
+    //test case 2: Single nummber
+    expect(stringCalculator.add("1")).toBe(1);
+})
